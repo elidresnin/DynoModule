1ea5de9580cfe966598db5291c9d6fdc6ef73af4
Eli Dresnin <eddres229@gmail.com>
2025-11-06 17:42:25 -0500

    Update DynoModule.ino

--- DynoModule.ino
+++ DynoModule.ino
@@ -1,7 +1,5 @@
 /* 
-
  DynoModule.ino 
-
 */
 
 // Import Librarys
@@ -14,10 +12,8 @@
 #include <Wire.h>
 #endif
 
-
 // U8G2 Display Constructor
 U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/13, /* data=*/11, /* cs=*/17, /* dc=*/16, /* reset=*/15);
-
 
 // Define Physical Pins
 #define ESTOP_PIN 2
@@ -27,27 +23,29 @@ U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/13, /* data=*/1
 #define ONOFF_PIN 6
 const int POTENTIOMETER_PIN = A0;
 
+// Declare State Variables
 bool first_start;
-
 
 // Declare current value global variables
 bool enable = false;
+bool send_can = false;
 bool disable = true;
+int potentiometerValue = 0;
 int torque_value = 0;
 bool reverse = false;
 
-// 
-void setup(void) {
 
+void setup(void) {
+  // Set pin modes (PULLUP = internal resistor)
   pinMode(FORREV_PIN, INPUT_PULLUP);
   pinMode(ESTOP_PIN, INPUT_PULLUP);
   pinMode(ENGINESTART_PIN, INPUT_PULLUP);
   pinMode(ONOFF_PIN, INPUT_PULLUP);
+  pinMode(ENGINESTART_PIN, INPUT_PULLUP);
 
+  // Initalize Screen
   u8g2.begin();
   first_start = true;
-
-  
 }
 
 
@@ -57,8 +55,14 @@ void loop(void) {
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_ncenB08_tr);
 
+  // Set global variables
+  reverse = digitalRead(FORREV_PIN);
+  enable = !digitalRead(ENABLE_PIN);
+  potentiometerValue = analogRead(POTENTIOMETER_PIN);
+  torque_value = map(potentiometerValue, 0, 1023, 0, 220);
+
   // Show first start graphic for 1 second
-  if(first_start == true) {
+  if(first_start) {
     u8g2.clearBuffer();
     u8g2.setCursor(0, 50);
     u8g2.print("TREV DYNO MODULE");
@@ -67,66 +71,31 @@ void loop(void) {
     first_start = false;
   }
 
-  // Set global variables
-  reverse = digitalRead(FORREV_PIN);
-  
-  enable = !digitalRead(ENABLE_PIN);
-  int potentiometerValue = analogRead(POTENTIOMETER_PIN);
-  torque_value = map(potentiometerValue, 0, 1023, 0, 220);
-
-
+  // ESTOP blocks the rest of the code from running
+  // TODO: Send CAN Message to disable
   if (digitalRead(ESTOP_PIN) == HIGH) {
     u8g2.clearBuffer();
     u8g2.setCursor(0, 50);
     u8g2.print("ESTOP!");
 
   } else {
-
+    // Torque
     u8g2.setCursor(0, 10);  // Set cursor position (x, y)
     u8g2.print("Torque Value: ");  // Print a string label
     u8g2.print(torque_value);  // write something to the internal memory
-    // Serial.print("Potentiometer Value: ");
-    // Serial.println(potentiometerValue);
-
-
-    
 
     // Reverse
     u8g2.setCursor(0, 20);
     u8g2.print(reverse ? "Direction: Reverse" : "Direction: Forward");
 
-    // enable
+    // Enable
     u8g2.setCursor(0, 30);
     u8g2.print(enable ? "Enabled" : "Disabled");
-    
 
-
-
-    // if (enable) {
-    //   u8g2.setCursor(0, 30);
-    //   u8g2.print("Enabled");
+    if (digitalRead(ENGINESTART_PIN) == HIGH) {
       
-
-    // } else {
-    //   u8g2.setCursor(0, 30);
-    //   u8g2.print("Disabled");
-      
-    // }
-
-
-
-    if (digitalRead(ONOFF_PIN) == HIGH) {
-
-      u8g2.setCursor(0, 50);
-
-      u8g2.print("Power OFF");
-
-    } else {
-
-      u8g2.setCursor(0, 50);
-
-      u8g2.print("Power ON");
     }
+    
   }
 
   u8g2.sendBuffer();  // transfer internal memory to the display
